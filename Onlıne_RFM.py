a90e5d07ce7507db70ff4ae10b870de4661b5a7f
Enes Fevzi <118919666+EnesFevzi@users.noreply.github.com>
2022-12-19 16:15:44 +0300

    Update Onlıne_RFM.py

--- Onlıne_RFM.py
+++ Onlıne_RFM.py
@@ -1,3 +1,17 @@
+############################################
+# CUSTOMER LIFETIME VALUE (Müşteri Yaşam Boyu Değeri)
+############################################
+
+# 1. Veri Hazırlama
+# 2. Average Order Value (average_order_value = total_price / total_transaction)
+# 3. Purchase Frequency (total_transaction / total_number_of_customers)
+# 4. Repeat Rate & Churn Rate (birden fazla alışveriş yapan müşteri sayısı / tüm müşteriler)
+# 5. Profit Margin (profit_margin =  total_price * 0.10)
+# 6. Customer Value (customer_value = average_order_value * purchase_frequency)
+# 7. Customer Lifetime Value (CLTV = (customer_value / churn_rate) x profit_margin)
+# 8. Segmentlerin Oluşturulması
+# 9. BONUS: Tüm İşlemlerin Fonksiyonlaştırılması
+
 import pandas as pd
 import datetime as dt
 from sklearn.preprocessing import MinMaxScaler
@@ -9,7 +23,7 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.float_format', lambda x: '%.2f' % x)
 pd.set_option('display.width',1000)
 
-df_ = pd.read_csv("C:/Users/Enes Fevzi/PycharmProjects/pythonProject4/CRM Analty/Rfm/flo_data_20k.csv")
+df_ = pd.read_csv("PycharmProjects/pythonProject4/CRM Analty/Rfm/flo_data_20k.csv")
 df = df_.copy()
 
 df.head(10)
